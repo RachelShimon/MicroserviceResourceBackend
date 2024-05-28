bd75d88a2635673e405495657b36551546aca4c0
Rachel Shimon <83765469+RachelShimon@users.noreply.github.com>
2024-05-28 15:32:12 +0300

    add cors support

--- src/main/java/checkmarx/microserviceresourcebackend/controllers/MicroserviceController.java
+++ src/main/java/checkmarx/microserviceresourcebackend/controllers/MicroserviceController.java
@@ -3,6 +3,7 @@ package checkmarx.microserviceresourcebackend.controllers;
 import checkmarx.microserviceresourcebackend.models.Microservice;
 import checkmarx.microserviceresourcebackend.services.IMicroservicesService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class MicroserviceController {
 
--- src/main/java/checkmarx/microserviceresourcebackend/controllers/ResourceController.java
+++ src/main/java/checkmarx/microserviceresourcebackend/controllers/ResourceController.java
@@ -4,6 +4,7 @@ import checkmarx.microserviceresourcebackend.models.Resource;
 import checkmarx.microserviceresourcebackend.services.IMicroservicesService;
 import checkmarx.microserviceresourcebackend.services.IResourceService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ResourceController {
 
